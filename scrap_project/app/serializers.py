2f441fe1223dc6367b2af386f0d37dd96b4a2c5c
abhay.nirmal <abhayyy1912@gmail.com>
2024-10-12 19:10:55 +0530

    moved to subdirectories

--- scrap_project/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import CellData
-
-class CellDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=CellData
-        fields='__all__'
--- scrap_project/app/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from app.views import CellDataList,CellDataViewSet
-urlpatterns = [
-    path('',CellDataList.as_view()),
-    path('upload',CellDataViewSet.as_view())
-]
